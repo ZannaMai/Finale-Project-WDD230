e034c85e85098706b4f89494a24daae4b9ca67d2
ZannaMai <97138655+ZannaMai@users.noreply.github.com>
2022-04-04 16:50:39 -0600

    json updated

--- js/weatherapi.js
+++ js/weatherapi.js
@@ -1,28 +1,19 @@
-const apiURL = "htttps://api.openweathermap.org/data/2.5/weather?temperature&lat=43.4931&lon=-112.0428&units=metric&appid=082ab106dd8a8209d0637abf917b514d";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?temperature&lat=38.98&lon=-77.09&units=imperial&appid=082ab106dd8a8209d0637abf917b514d";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+
     let temp = document.querySelector('#current-temp');
     temp.textContent = jsObject.main.temp.toFixed(1);
-    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-    const desc = jsObject.weather[0].description;
-    const windspeed = jsObject.wind.speed.toFixed(1);
-    const windchill = jsObject.main.feels_like.toFixed(1);
 
-    document.querySelector('#weathericon').setAttribute('src', iconsrc);
-    document.querySelector('#weathericon').setAttribute('alt', desc);
-    document.querySelector('figcaption').textContent = desc;
-    document.querySelector('#windspeed').textContent = windspeed;
-    document.querySelector("#windchill").textContent = windchill;
+    let humidity = document.querySelector('#humidity');
+    humidity.textContent = jsObject.main.humidity.toFixed(1);
+
+    let conditions = document.querySelector('#conditions');
+    conditions.textContent = jsObject.weather.main.toFixed();
+
+    myMap.setWeatherAlertVisibility(true);
+    const alertVisible = myMap.isWeatherAlertVisible();
+    myMap.toggleWeatherAlertVisibility();
   });
-
-const windChill = (celsiusTemp, windS) => {
-  return (celsiusTemp <= 10 && windS > 4.8)
-  ?
-    Math.round(0.6215 * celsiusTemp - 11.37 *
-      Math.pow(windS, 0.16) + 0.3965 * celsiusTemp * 
-      Math.pow(windS, 0.16) + 13.12)
-  :
-    "N/A";
-}
